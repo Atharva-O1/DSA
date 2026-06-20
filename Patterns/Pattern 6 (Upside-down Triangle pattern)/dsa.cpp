84498fa272aab46752ca328925546cb08bdc8f55
Atharva-O1 <subhashramteke7@gmail.com>
2026-06-20 22:29:28 +0530

    new pattern added

---
+++ Patterns/Pattern 6 (Upside-down Triangle pattern)/dsa.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	
+	int i = 1;
+	while(i <= n){
+		
+	int j = n - i + 1;
+	while(j){
+		cout << "*";
+		j--;
+	}
+	
+	int space = 1;
+	while(space <= i){
+		cout << " ";
+		space++;
+	}
+	cout << endl;
+	i++;
+	}
+}
