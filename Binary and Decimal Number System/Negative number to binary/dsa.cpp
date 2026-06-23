4615de87a4f09b296b4e058313888ac1090832d0
Atharva-O1 <subhashramteke7@gmail.com>
2026-06-23 23:33:56 +0530

    negative to binary

---
+++ Binary and Decimal Number System/Negative number to binary/dsa.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int ans = 0;
+    int i = 0;
+    
+    if (n > 0) {
+        while (n != 0) {
+            int bit = n & 1;
+            ans = (bit * pow(10, i)) + ans;
+            n = n >> 1;
+            i++;
+        } 
+    } 
+    else if (n < 0) {
+        n = n * -1;
+        while (n != 0) {
+            int bit = n & 1;
+            ans = (bit * pow(10, i)) + ans;
+            n = n >> 1;
+            i++;    
+        }
+    } 
+    
+    cout << ans << endl; 
+    return 0;
+}
+
+
